b29109b06f50200d7d25364be81a5e8236b68bf9
ganggyunggyu <qwzx16@naver.com>
2025-08-13 18:02:27 +0900

    feat: gpt class add

--- constants/Model.py
+++ constants/Model.py
@@ -1,5 +1,6 @@
 class Model:
-    GPT5_MINI = "gpt-5-mini-2025-08-07"
     GPT5 = "gpt-5-2025-08-07"
+    GPT5_MINI = "gpt-5-mini-2025-08-07"
+    GPT5_NANO = "gpt-5-nano-2025-08-07"
     GPT4_1 = "gpt-4.1-2025-04-14"
     GPT4_1_MINI = "gpt-4.1-mini-2025-04-14"
