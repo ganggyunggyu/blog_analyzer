7c88ebf8408f330dab3b0252b75ba57cdd49aaf3
ganggyunggyu <qwzx16@naver.com>
2025-09-02 16:40:45 +0900

    claude test

--- llm/claude_service.py
+++ llm/claude_service.py
@@ -1,56 +1,9 @@
 import anthropic
 import os
+from llm._claude_file_uploader import get_file_ids
 from _prompts.get_claude_prompts import ClaudePrompt
 from config import CLAUDE_API_KEY
 from enum import Enum
-
-
-def file_upload(file_name):
-    client = anthropic.Anthropic(api_key=CLAUDE_API_KEY)
-    client.beta.files.upload(
-        file=(
-            f"{file_name}.txt",
-            open(
-                f"/Users/ganggyunggyu/Programing/21lab/blog_analyzer/_docs/claude_docs/{file_name}.txt",
-                "rb",
-            ),
-            "text/plain",
-        ),
-    )
-
-
-def get_file_list():
-    import anthropic
-
-    client = anthropic.Anthropic(api_key=CLAUDE_API_KEY)
-    files = client.beta.files.list()
-    print(files)
-    return files
-
-
-def get_file_ids():
-    import anthropic
-
-    client = anthropic.Anthropic(api_key=CLAUDE_API_KEY)
-    all_ids = []
-    starting_after = None
-
-    while True:
-        response = client.beta.files.list(
-            limit=1,
-        )
-        files = response.data
-
-        if not files:
-            break
-
-        all_ids.extend([f.id for f in files])
-        starting_after = files[-1].id  # 다음 페이지로 이동
-
-        if not response.has_more:  # 더 이상 없으면 중단
-            break
-
-    return all_ids
 
 
 class ClaudeModel(Enum):
