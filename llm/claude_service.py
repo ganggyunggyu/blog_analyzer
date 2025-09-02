a1119a420bcd063b34370dc43cb0cb76f5612f91
ganggyunggyu <qwzx16@naver.com>
2025-09-02 16:23:13 +0900

    fix

--- llm/claude_service.py
+++ llm/claude_service.py
@@ -2,8 +2,55 @@ import anthropic
 import os
 from _prompts.get_claude_prompts import ClaudePrompt
 from config import CLAUDE_API_KEY
-from _claude_file_uploader import get_file_ids
 from enum import Enum
+
+
+def file_upload(file_name):
+    client = anthropic.Anthropic(api_key=CLAUDE_API_KEY)
+    client.beta.files.upload(
+        file=(
+            f"{file_name}.txt",
+            open(
+                f"/Users/ganggyunggyu/Programing/21lab/blog_analyzer/_docs/claude_docs/{file_name}.txt",
+                "rb",
+            ),
+            "text/plain",
+        ),
+    )
+
+
+def get_file_list():
+    import anthropic
+
+    client = anthropic.Anthropic(api_key=CLAUDE_API_KEY)
+    files = client.beta.files.list()
+    print(files)
+    return files
+
+
+def get_file_ids():
+    import anthropic
+
+    client = anthropic.Anthropic(api_key=CLAUDE_API_KEY)
+    all_ids = []
+    starting_after = None
+
+    while True:
+        response = client.beta.files.list(
+            limit=1,
+        )
+        files = response.data
+
+        if not files:
+            break
+
+        all_ids.extend([f.id for f in files])
+        starting_after = files[-1].id  # 다음 페이지로 이동
+
+        if not response.has_more:  # 더 이상 없으면 중단
+            break
+
+    return all_ids
 
 
 class ClaudeModel(Enum):
@@ -20,7 +67,7 @@ from anthropic._exceptions import BadRequestError, RateLimitError
 def claude_gen(keyword: str, ref: str = "") -> str:
     user = ClaudePrompt.get_user_prompt()
     system = ClaudePrompt.get_system_prompt()
-    model = ClaudeModel.SONNET_3_7.value
+    model = ClaudeModel.SONNET_4.value
 
     client = anthropic.Anthropic(api_key=CLAUDE_API_KEY)
     file_ids = get_file_ids()
@@ -37,7 +84,6 @@ def claude_gen(keyword: str, ref: str = "") -> str:
     ]
 
     prompt = f"""
-{user}
 
 [키워드]
 {keyword}
@@ -57,7 +103,7 @@ def claude_gen(keyword: str, ref: str = "") -> str:
                     "content": [
                         {
                             "type": "text",
-                            "text": prompt,
+                            "text": "문서들 분석해줘",
                         },
                         *document_blocks,
                     ],
