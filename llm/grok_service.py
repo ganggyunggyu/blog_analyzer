3be5a3012ff1d7573e227632275e82736da9bb3a
ganggyunggyu <qwzx16@naver.com>
2026-01-09 10:18:31 +0900

    fix

--- llm/grok_service.py
+++ llm/grok_service.py
@@ -12,7 +12,7 @@ from utils.text_cleaner import comprehensive_text_clean
 from utils.ai_client_factory import call_ai
 
 
-MODEL_NAME: str = Model.DEEPSEEK_RES
+MODEL_NAME: str = Model.GROK_4_NON_RES
 
 
 def grok_gen(user_instructions: str, ref: str = "", category: str = "") -> str:
