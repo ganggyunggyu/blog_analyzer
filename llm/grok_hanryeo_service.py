ecf577c3050260b347693786eae64b6a6b6e940b
ganggyunggyu <qwzx16@naver.com>
2026-04-27 09:41:59 +0900

    fix: switch grok hanryeo model to deepseek flash

--- llm/grok_hanryeo_service.py
+++ llm/grok_hanryeo_service.py
@@ -11,7 +11,7 @@ from utils.text_cleaner import comprehensive_text_clean
 from utils.ai_client_factory import call_ai
 
 
-MODEL_NAME: str = Model.GROK_4_NON_RES
+MODEL_NAME: str = Model.DEEPSEEK_V4_FLASH
 
 
 def grok_hanryeo_gen(user_instructions: str, ref: str = "", category: str = "") -> str:
